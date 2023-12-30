bfb9d6ca59d7eecee5f85d579019ca86b2616e4a
zntb <zenetibi@yahoo.com>
2023-12-30 09:29:14 +0200

    Move the loader() function in the Events component code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import RootLayout from './pages/Root';
 import HomePage from './pages/Home';
-import EventsPage from './pages/Events';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EditEventPage from './pages/EditEvent';
 import EventDetailPage from './pages/EventDetail';
 import NewEventPage from './pages/NewEvent';
@@ -21,16 +21,7 @@ const router = createBrowserRouter([
           {
             index: true,
             element: <EventsPage />,
-            loader: async () => {
-              const response = await fetch('http://localhost:8080/events');
-
-              if (!response.ok) {
-                //
-              } else {
-                const resData = await response.json();
-                return resData.events;
-              }
-            },
+            loader: eventsLoader,
           },
           { path: ':eventId', element: <EventDetailPage /> },
           { path: 'new', element: <NewEventPage /> },
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -12,4 +12,15 @@ function EventsPage() {
   );
 }
 
+export async function loader() {
+  const response = await fetch('http://localhost:8080/events');
+
+  if (!response.ok) {
+    //
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
+
 export default EventsPage;
