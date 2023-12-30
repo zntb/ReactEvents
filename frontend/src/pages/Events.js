2ed0923b6356042d4e95fcdfde2190715c7a6c32
zntb <zenetibi@yahoo.com>
2023-12-30 09:55:30 +0200

    Returning Responses in loader()s

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -3,7 +3,8 @@ import { useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  const events = data.events;
 
   return (
     <>
@@ -18,8 +19,7 @@ export async function loader() {
   if (!response.ok) {
     //
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 }
 
