a9febe7075fff0aa4e6a4c08183a77f9ffb373de
zntb <zenetibi@yahoo.com>
2023-12-30 15:06:39 +0200

    Extracting Error Data & Throwing Responses

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -19,11 +19,13 @@ function EventsPage() {
 }
 
 export async function loader() {
-  const response = await fetch('http://localhost:8080/eventsssss');
+  const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
     // return { isError: true, message: 'Could not fetch events.' };
-    throw { message: 'Could not fetch events.' };
+    throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+      status: 500,
+    });
   } else {
     return response;
   }
