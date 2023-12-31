15d38bc0f82d36298735bde5827b92cdcddcee76
zntb <zenetibi@yahoo.com>
2023-12-31 08:21:34 +0200

    Add Dynamic Routes & loader()s

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,9 @@ import RootLayout from './pages/Root';
 import HomePage from './pages/Home';
 import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EditEventPage from './pages/EditEvent';
-import EventDetailPage from './pages/EventDetail';
+import EventDetailPage, {
+  loader as eventDetailLoader,
+} from './pages/EventDetail';
 import NewEventPage from './pages/NewEvent';
 import EventsRootLayout from './pages/EventsRoot';
 import ErrorPage from './pages/Error';
@@ -25,7 +27,11 @@ const router = createBrowserRouter([
             element: <EventsPage />,
             loader: eventsLoader,
           },
-          { path: ':eventId', element: <EventDetailPage /> },
+          {
+            path: ':eventId',
+            element: <EventDetailPage />,
+            loader: eventDetailLoader,
+          },
           { path: 'new', element: <NewEventPage /> },
           { path: ':eventId/edit', element: <EditEventPage /> },
         ],
