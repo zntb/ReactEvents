0db82cd8671aaf2c9fb505cc680dde462e431f75
zntb <zenetibi@yahoo.com>
2023-12-31 07:50:16 +0200

    Add The json() Utility Function

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from 'react-router-dom';
+import { json, useLoaderData } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
 
@@ -11,24 +11,21 @@ function EventsPage() {
 
   const events = data.events;
 
-  return (
-    <>
-      <EventsList events={events} />
-    </>
-  );
+  return <EventsList events={events} />;
 }
+
+export default EventsPage;
 
 export async function loader() {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
     // return { isError: true, message: 'Could not fetch events.' };
-    throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
-      status: 500,
-    });
+    // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+    //   status: 500,
+    // });
+    throw json({ message: 'Could not fetch events.' }, { status: 500 });
   } else {
     return response;
   }
 }
-
-export default EventsPage;
