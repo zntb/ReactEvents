35c8b17c64b306629427b0a83465645d955f5eb2
zntb <zenetibi@yahoo.com>
2024-01-01 10:51:16 +0200

    Add style to Home Page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,9 @@
+import classes from '../components/PageContent.module.css';
+
 function HomePage() {
   return (
     <>
-      <h1>Home Page</h1>
+      <h1 className={classes.content}>Home Page</h1>
     </>
   );
 }
